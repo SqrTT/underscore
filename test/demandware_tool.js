58b9d96bbd48f24c939414e571cd63a69a6a8410
SqrTT <tolik@sqrtt.pro>
2015-06-30 22:27:30 +0300

    fixed formating

--- test/demandware_tool.js
+++ test/demandware_tool.js
@@ -3,17 +3,17 @@
 
   QUnit.module('Demandware tool');
   test('labmda', function() {
-  	var testArr = [1, 2, 3, 4, 5];
+    var testArr = [1, 2, 3, 4, 5];
     strictEqual(_.filter(testArr, '-> $ <= 2').length, 2,
     	'should filter element');
     strictEqual(_.filter(testArr, 'val -> val == 2').length, 1,
     	'should filter element with value name');
-  	strictEqual(_.filter(testArr, '(val, idx) -> idx == 3').length, 1,
+    strictEqual(_.filter(testArr, '(val, idx) -> idx == 3').length, 1,
   		'should filter element with second value name works too');
-  	testArr[5] = { complex: { obj: {value: 1}}};
-  	strictEqual(_.filter(testArr, ' val -> _.prop(val, "complex.obj.value")').length, 1,
+    testArr[5] = {complex: {obj: {value: 1}}};
+    strictEqual(_.filter(testArr, ' val -> _.prop(val, "complex.obj.value")').length, 1,
   		'should filter element with underscore in scope');
-  }); 
+  });
 
 }());
 
